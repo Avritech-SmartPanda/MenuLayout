f7c076a1182b9cfc2eddc54c3956ea63e6f063f8
lesly Phiri <phirilesley@outlook.com>
2022-08-01 16:39:37 +0200

    dyanamic menu

---
+++ Shared/Menu.cs
@@ -0,0 +1,19 @@
+﻿namespace MenuLayout.Shared
+{
+    public class Menu
+    {
+        public string? Icon { get; set; }
+        public string? Title { get; set; }
+        public string? Subtitle { get; set; }
+        public string? Link { get; set; }
+        public List<ChildMenu>? Children { get; set; }
+    }
+
+    public class ChildMenu
+    {
+        public string? Icon { get; set; }
+        public string? Title { get; set; }
+        public string? Link { get; set; }
+        public string? Subtitle { get; set; }
+    }
+}
